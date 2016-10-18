9fbd1e210425ab12d6894f1f124c896be19a0c8c
Danny McClanahan <danieldmcclanahan@gmail.com>
2016-10-18 18:44:44 -0500

    add today's lecture!

---
+++ 2016-10-18-place-picker/lecture.md
@@ -0,0 +1,21 @@
+nearby places
+=============
+
+## startActivityForResult
+- starts another activity with an intent, other activity "returns" intent
+- call `startActivityForResult` to do this with explicit or implicit intent
+- override `onActivityResult` to receive result
+
+## google place picker
+- uses places api
+- use an api key i made: AIzaSyBGd38Atou3a6zFzLWkYIWKp
+    - please don't use this to spam the service
+- opens separate activity to pick location, returns to calling activity
+
+## today
+- open place picker in a separate activity and display the result on the screen!
+- you can start off with the code in [start](start/)
+- my finished version will be available in [end](end/) after the class is finished
+
+## result
+![the app in use](app.gif)
---
+++ 2016-10-18-place-picker/start/app/src/main/java/com/example/cosmicexplorer/getlocationfrommap/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cosmicexplorer.getlocationfrommap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.vision.text.Text;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startPlacePicker(View view) {
+        // TODO: start place picker here!
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO: handle result here!
+    }
+}
